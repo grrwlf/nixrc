33ffa67ba2ff1a3bbd64c7d610ab475b0f479c61
Sergey Mironov <grrwlf@gmail.com>
2013-07-16 14:11:27 +0400

    cfg/samsung-np900x3c-v2.nix: Updated

--- cfg/samsung-np900x3c-v2.nix
+++ cfg/samsung-np900x3c-v2.nix
@@ -209,6 +209,15 @@ rec {
     };
   };
 
+  environment.promptInit = ''
+    PROMPT_COLOR="1;31m"
+    let $UID && PROMPT_COLOR="1;32m"
+    PS1="\n\[\033[$PROMPT_COLOR\][\u@\h \w ]\\$\[\033[0m\] "
+    if test "$TERM" = "xterm"; then
+      PS1="\[\033]2;\h:\u: \w\007\ ]$PS1"
+    fi
+  '';
+
   environment.systemPackages = with pkgs ; [
 
     # Basic tools
